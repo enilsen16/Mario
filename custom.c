b294c90a2f3f8f17d6bddad5967cc6e1a982da15
Erik Nilsen <enilsen16@live.com>
2014-10-23 09:47:30 -0700

    add loop

--- custom.c
+++ custom.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-  printf("hello, David\n");
+  printf("hello, Marco\n");
+
+for (int i = 0; i < 10; i++)
+  {
+    printf("hello, World!\n");
+  }
+
+
 }
